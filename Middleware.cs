a9e705a1d3f810afc8035eb245255effddb3c5e7
Uira Lima <uiralima@gmail.com>
2021-03-17 09:01:46 -0300

    first commit

---
+++ Middleware.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfresponsability.Framework
+{
+    public abstract class Middleware
+    {
+        protected abstract void Execute(AppHttpContext context);
+
+        public void Act(AppHttpContext context)
+        {
+            Execute(context);
+        }
+
+        protected void Next(AppHttpContext context)
+        {
+            Middleware next = context.Next();
+            if (null != next)
+            {
+                next.Execute(context);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
